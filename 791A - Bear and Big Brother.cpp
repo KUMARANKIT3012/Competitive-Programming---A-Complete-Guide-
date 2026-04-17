715df4712be24f79855035ecdf4223d09c527c58
Ankit Kumar <ankitkumar16729@gmail.com>
2026-04-17 21:10:10 +0530

    Add solution for Bear and Big Brother problem

---
+++ 791A - Bear and Big Brother.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int a, b;
+    cin>>a>>b;
+    int result = 0;
+    
+    while(b >= a){
+        a = a*3;
+        b = b*2;
+        result++;
+    }
+    
+    cout<<result;
+}
