efb754748b0229000dd92631895f0ddfb04910d5
Ankit Kumar <ankitkumar16729@gmail.com>
2026-05-23 11:28:32 +0530

    Implement Anton and Letters solution

---
+++ 443A - Anton and Letters.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <set>
+using namespace std;
+
+int main() {
+    
+    string s;
+    getline(cin, s);
+
+    set<char> st;
+
+    for (char ch : s) {
+        if (ch >= 'a' && ch <= 'z') {
+            st.insert(ch);
+        }
+    }
+
+    cout << st.size();
+
+    return 0;
+}
