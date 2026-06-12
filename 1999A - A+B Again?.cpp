8572eabc1ee8d6a77d2d5f4b540699b62f1bb9fc
Ankit Kumar <ankitkumar16729@gmail.com>
2026-06-12 12:08:33 +0530

    Add solution for 'A+B Again?' problem

---
+++ 1999A - A+B Again?.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int sum = n / 10 + n % 10;
+        cout << sum << '\n';
+    }
+
+    return 0;
+}
