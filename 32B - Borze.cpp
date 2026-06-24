9d42301a69a88157ae5d9b472a897df831a08ac8
Ankit Kumar <ankitkumar16729@gmail.com>
2026-06-24 14:15:33 +0530

    Implement Borze encoding in 32B - Borze.cpp

---
+++ 32B - Borze.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    string s;
+    cin >> s;
+
+    string ans;
+
+    for (int i = 0; i < s.size();) {
+        if (s[i] == '.') {
+            ans += '0';
+            i++;
+        } else {
+            if (s[i + 1] == '.') {
+                ans += '1';
+            } else {
+                ans += '2';
+            }
+            i += 2;
+        }
+    }
+
+    cout << ans;
+
+    return 0;
+}
