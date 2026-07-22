571c8df945a1fc2639db2919178bafc577007710
Ankit Kumar <ankitkumar16729@gmail.com>
2026-07-22 10:27:18 +0530

    Add solution for Balanced Array problem
    Implement solution for the Balanced Array problem, checking if n is divisible by 4 and generating the required output.

---
+++ 1343B - Balanced Array.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    ios::sync_with_stdio(false);
+    cin.tie(nullptr);
+
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        if (n % 4 != 0) {
+            cout << "NO\n";
+            continue;
+        }
+
+        cout << "YES\n";
+
+        int k = n / 2;
+
+        for (int i = 1; i <= k; i++)
+            cout << 2 * i << " ";
+
+        for (int i = 1; i < k; i++)
+            cout << 2 * i - 1 << " ";
+
+        cout << 3 * k - 1 << "\n";
+    }
+
+    return 0;
+}
