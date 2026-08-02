b5fef00c8f49f4ae73654bf9a0fde9d2d2f212f8
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-02 13:36:37 +0530

    Create 1742B - Increasing.cpp

---
+++ 1742B - Increasing.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+
+        for (int i = 0; i < n; i++)
+            cin >> a[i];
+
+        sort(a.begin(), a.end());
+
+        bool ok = true;
+
+        for (int i = 1; i < n; i++) {
+            if (a[i] == a[i - 1]) {
+                ok = false;
+                break;
+            }
+        }
+
+        if (ok)
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
