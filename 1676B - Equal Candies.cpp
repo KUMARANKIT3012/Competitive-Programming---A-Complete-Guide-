c0a37eea985d587d93384b967ed8dfd185c246e1
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-04 19:26:01 +0530

    Create 1676B - Equal Candies.cpp

---
+++ 1676B - Equal Candies.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> a(n);
+        int mn = INT_MAX;
+
+        for (int i = 0; i < n; i++) {
+            cin >> a[i];
+            mn = min(mn, a[i]);
+        }
+
+        long long ans = 0;
+        for (int i = 0; i < n; i++) {
+            ans += (a[i] - mn);
+        }
+
+        cout << ans << "\n";
+    }
+
+    return 0;
+}
