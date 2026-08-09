d2fa11ef50101313f2eab32ac3a45a42a041094f
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-09 10:32:40 +0530

    Add solution for Fair Division problem

---
+++ 1472B - Fair Division.cpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int ones = 0, twos = 0;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+
+            if (x == 1)
+                ones++;
+            else
+                twos++;
+        }
+
+        if (ones % 2 == 0 && (twos % 2 == 0 || ones >= 2))
+            cout << "YES\n";
+        else
+            cout << "NO\n";
+    }
+
+    return 0;
+}
