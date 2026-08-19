6cafb56334128124f3d531cf26763b3611b43246
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-19 15:30:47 +0530

    Create 2254A - Riptide.cpp

---
+++ 2254A - Riptide.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int a, b, c;
+        cin >> a >> b >> c;
+
+        int ans = 0;
+
+        while (a != b && b != c && a != c) {
+            int mn = min({a, b, c});
+            int mx = max({a, b, c});
+
+            if (a == mn) a++;
+            else if (b == mn) b++;
+            else c++;
+
+            if (a == mx) a--;
+            else if (b == mx) b--;
+            else c--;
+
+            ans++;
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
