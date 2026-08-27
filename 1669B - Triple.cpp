04b5f4d34bd8a0ea8cfbb31c973aa2bbffc36b88
Ankit Kumar <ankitkumar16729@gmail.com>
2026-08-27 12:42:29 +0530

    Implement solution for Triple problem

---
+++ 1669B - Triple.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        vector<int> freq(n + 1, 0);
+        int ans = -1;
+
+        for (int i = 0; i < n; i++) {
+            int x;
+            cin >> x;
+            freq[x]++;
+
+            if (freq[x] == 3)
+                ans = x;
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
