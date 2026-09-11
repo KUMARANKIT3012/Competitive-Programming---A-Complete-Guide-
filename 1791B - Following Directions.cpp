a45a42c3ed1c0856d8c125dd48c9d22121d0c4eb
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-11 17:08:34 +0530

    Create 1791B - Following Directions.cpp

---
+++ 1791B - Following Directions.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        string s;
+        cin >> s;
+
+        int x = 0, y = 0;
+        bool found = false;
+
+        for (char c : s) {
+            if (c == 'L') x--;
+            else if (c == 'R') x++;
+            else if (c == 'U') y++;
+            else if (c == 'D') y--;
+
+            if (x == 1 && y == 1)
+                found = true;
+        }
+
+        cout << (found ? "YES" : "NO") << '\n';
+    }
+
+    return 0;
+}
