3c6a18a2518ef40ddac0501b87f7f9681d770b4c
Ankit Kumar <ankitkumar16729@gmail.com>
2026-09-22 16:35:51 +0530

    Add solution for Ten Words of Wisdom problem
    Implement solution for the 'Ten Words of Wisdom' problem.

---
+++ 1850B - Ten Words of Wisdom.cpp
@@ -0,0 +1,29 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    int t;
+    cin >> t;
+
+    while (t--) {
+        int n;
+        cin >> n;
+
+        int best = -1;
+        int ans = -1;
+
+        for (int i = 1; i <= n; i++) {
+            int a, b;
+            cin >> a >> b;
+
+            if (a <= 10 && b > best) {
+                best = b;
+                ans = i;
+            }
+        }
+
+        cout << ans << '\n';
+    }
+
+    return 0;
+}
